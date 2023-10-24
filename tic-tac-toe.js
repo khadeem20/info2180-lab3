2255dd7c4fcb0ca1155cb2d36a48c726339c1521
Khadeem Lattibeaudiere <khadeemlatti20@gmail.com>
2023-10-23 20:38:54 -0400

    modification to js file

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,37 @@
+//exercise 1
 document.addEventListener("DOMContentLoaded", function(){
+    var board = document.getElementById("board");
+    var squares = board.querySelectorAll("div");
 
-        var Board = document.getElementById("board");
-
-        var InnerDivs = board.querySelectorAll("div");
-        
-        for (i=0;i < InnerDivs.length ; i++){
-            InnerDivs[i].classList.add("square");
-        };
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].classList.add("square");
     }
-);
+
+    var currentVal = "";
+    var alt= "";
+    const state = ['','','',
+                   '','','',         
+                   '','',''];
+    squares.forEach(function(square, index) {
+        square.addEventListener('click', function() {
+            if (!square.classList.contains("X") && !square.classList.contains("O")) {
+                square.classList.add("X");
+                square.textContent = "X";
+                currentVal = square.classList.value;
+            }
+
+            else {
+                currentVal = currentVal === "X" ? "O" : "X";
+                alt = currentVal === "X" ? "O" : "X";
+                square.classList.remove(alt);
+                square.classList.add(currentVal);
+                square.textContent = currentVal;
+                //currentVal = square.classList.value;
+            }
+            
+            state[index] = currentVal;
+
+            }
+        );
+    });
+});
